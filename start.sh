296f1261de3d86ce24f8d39676efdc55b9d64bb3
bastien-gillon <bastien.gillon@protonmail.com>
2020-06-19 15:57:26 +0200

    add in playbook

--- start.sh
+++ start.sh
@@ -3,15 +3,9 @@
 echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 
 if [ $? -eq 0 ]; then 
-    
-    dpkg -s git # check git a faire 
-    dpkg -s ansible #check ansible a faire
-
-    if [ nogit ]; then 
-        pacman -Sy git --noconfirm
-    else if  [ noansible ]; then
-        pacman -Sy git --noconfirm
-    fi
+   
+    pacman -Sy git --noconfirm
+    pacman -Sy git --noconfirm
 
     ansible-playbook playbook.yml
 
