d97bc28caeac4d1a9ea936e45f729fa07a75e052
bastien-gillon <bastien.gillon@protonmail.com>
2020-06-19 17:04:12 +0200

    a

--- start.sh
+++ start.sh
@@ -5,9 +5,10 @@ echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
 if [ $? -eq 0 ]; then 
    
     pacman -Sy git --noconfirm
-    pacman -Sy git --noconfirm
+    pacman -Sy ansible --noconfirm
 
     ansible-playbook playbook.yml
+    ansible-playbook -i vyos.example.net, -u ansible -k -e ansible_network_os=vyos first_playbook.yml
 
 else
     echo "Offline : please make sure that you have a internet connection"
